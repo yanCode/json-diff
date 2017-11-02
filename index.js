1dc3cc09df9919aff1f3f5b644fb65cbc6d4ed72
ShengYan, Zhang <yancode@qq.com>
2017-11-02 09:20:30 +0800

    compare the value

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const _ = require('lodash');
 const request = require('request');
 const fs = require('fs');
 const jstringify = require('json-stringify');
+const jsonPath = require('jsonpath');
 const exec  = require('child_process').exec;
+const writeFile = Promise.promisify(fs.writeFile);
 
 const apiOne = '',
   apiTwo = '',
@@ -17,17 +19,26 @@ function getJsonFile(url, filename) {
       if(err){
         return reject(err);
       }
-      fs.writeFile(filename, jstringify(body, null, 2, {offset: 4}), function (error) {
-        if(error){
-          return reject(error);
-        }
-        return resolve(filename);
-      })
+      return resolve(body);
     })
   })
 }
 
 
-Promise.join(getJsonFile(apiOne + parameter, 'production.json'), getJsonFile(apiTwo + parameter, 'test.json')).then(function () {
+Promise.join(getJsonFile(apiOne + parameter, 'production.json'), getJsonFile(apiTwo + parameter, 'test.json')).then(function (results) {
+  const [productJson, testJson] = results;
+  const countPattern = '$.categoryHistogram[*].count';
+  const fullPricePattern = '$.categoryHistogram[*].price..fullPrice';
+  return {productJson, testJson, result: _.isEqual(jsonPath.query(productJson, countPattern), jsonPath.query(testJson, countPattern))
+  && _.isEqual(jsonPath.query(productJson, fullPricePattern), jsonPath.query(testJson, fullPricePattern))};
+}).then(({productJson, testJson, result})=>{
+  if(!result){
+    console.log(`the results of ${parameter} are identical`);
+    process.exit(0);
+  }
+ return  Promise.join(writeFile('production.json',  jstringify(productJson, null, 2, {offset: 4})),
+   writeFile('test.json', jstringify(testJson, null, 2, {offset: 4})));
+}).then(function () {
   exec('kankan diff production.json test.json');
-})
+
+});
